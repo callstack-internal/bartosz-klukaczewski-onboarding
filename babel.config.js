57781a999be946d1d1f3414c2615d02067143cc0
Bartosz Klukaczewski <bklukaczewski@gmail.com>
2023-11-14 23:19:49 +0100

    fix: remove unused dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        extensions: [
-          '.ios.js',
-          '.ios.ts',
-          '.ios.tsx',
-          '.android.js',
-          '.android.ts',
-          '.android.tsx',
-          '.js',
-          '.ts',
-          '.tsx',
-          '.json',
-        ],
-      },
-    ],
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        runtime: 'automatic',
-      },
-    ],
-  ],
 };
